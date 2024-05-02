b830cbf2fd251a72ff605322b250d124a0bdeb40
Nicholas Folis <heffawoozle77@gmail.com>
2024-05-02 00:32:22 -0500

    I fixed some links, and I have added other pages.

--- devportfolio/urls.py
+++ devportfolio/urls.py
@@ -21,6 +21,9 @@ from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home),
-    path("", RedirectView.as_view(url="home/", permanent=True))
+    path('home/', views.home, name="home"),
+    path("", RedirectView.as_view(url="home/", permanent=True)),
+    path('about/', views.about),
+    path('work/', views.work),
+    path('contact/', views.contact),
 ]
