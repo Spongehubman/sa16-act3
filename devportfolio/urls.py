8054b677282eba01c2a74d9b1b9641d47fef7b02
Nicholas Folis <heffawoozle77@gmail.com>
2024-05-01 19:55:18 -0500

    In devportfolio/urls.py, I added a redirect link function referring to the 'home' page.

--- devportfolio/urls.py
+++ devportfolio/urls.py
@@ -20,4 +20,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home', views.home),
+    path("", RedirectView.as_view(url="home/", permanent=True))
 ]
