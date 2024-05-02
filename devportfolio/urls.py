9ad7ce64c22a50f9906018fdfa7531f2897277c1
Nicholas Folis <heffawoozle77@gmail.com>
2024-05-01 21:11:13 -0500

    I added two import statements to devportfolio/urls.py, and I migrated the web app successfully, which added new necessary files.

--- devportfolio/urls.py
+++ devportfolio/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import RedirectView
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
